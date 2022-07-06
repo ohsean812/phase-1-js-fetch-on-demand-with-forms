80b92e82ad58c55a0179f5db5bc7cb748f3ae3a5
Sean Oh <ohsean812@gmail.com>
2022-07-06 01:05:11 -0400

    commit tree bd57c8100a4e957f11fd3236ea29258529b09678

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 const init = () => {
-  
+
+    //Access the form
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const movieID = document.getElementById("searchByID").value;
+
+        if(movieID === "") alert("Error: Empty input. Please enter a value!");
+        else{
+            fetch(`http://localhost:3000/movies/${movieID}`)
+            .then((response) => {
+                if(response.status === 404) return response.status;
+                else return response.json();
+            })
+            .then((movie) => {
+                console.log(movie);
+                // If we get a 404 error, user sees an error alert
+                if(movie === 404){
+                    alert("Error: Page not found!");
+                }
+                // Otherwise, we update the title and summary as expected
+                else{
+                    const title = document.querySelector("section#movieDetails h4");
+                    const summary = document.querySelector("section#movieDetails p");
+                    title.innerText = movie.title;
+                    summary.innerText = movie.summary;
+                }
+            })
+        }
+    })   
 }
 
 document.addEventListener('DOMContentLoaded', init);
